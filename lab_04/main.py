493cd62c55b823688967b01bf9df0f3d58b1aacb
wh75er <bokseng12@gmail.com>
2018-04-15 22:50:09 +0300

    lab4 had been finished

--- lab_04/main.py
+++ lab_04/main.py
@@ -1,27 +1,97 @@
+from math import *
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
+def f(x, j):
+    return pow(x, j)
+
+def approximate(x, coefs):
+    y = 0
+    for j in range(n + 1):
+        y += coefs[j] * f(x, j)
+        
+    return y
+
+def nparray_to_list(a):
+    a = list(a)
+    for i in range(len(a)):
+        a[i] = list(a[i])
+    return a
+
+def gauss_seidel(A, b, eps):
+    n = len(A)  
+    x = [0 for i in range(n)]
+
+    converge = False
+    while not converge:
+        x_new = x.copy()
+        for i in range(n):
+            s1 = sum(A[i][j] * x_new[j] for j in range(i))
+            s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
+            x_new[i] = (b[i] - s1 - s2) / A[i][i]
+
+        converge = sqrt(sum((x_new[i] - x[i]) ** 2 for i in range(n))) <= eps
+        x = x_new
+    return x
+
+def solve():
+    N = len(x)
+    A = np.zeros((n + 1, n + 1))
+    B = np.zeros((n + 1, 1))
+    for j in range(n + 1):
+        for k in range(n + 1):
+            for i in range(N):
+                A[j, k] += f(x[i], k + j) * p[i]
+        for i in range(N):
+            B[j] += y[i] * f(x[i], j) * p[i]
+
+    det = np.linalg.det(A)
+    
+    nparray_to_list(A)
+    nparray_to_list(B)
+    
+    print("determinant: ", det)
+    if abs(det) <= 1e-7:
+        print("\nDeterminant is NULL!\n")
+        exit()
+
+    return gauss_seidel(A, B, 1e-4)
 
 
-def inputData():
-    f = open("data.txt", "r");
 
-    order = int(input("Please, input polinomial order: "))
+x = []
+y = []
+p = []
 
-    x = []
-    y = []
-    weights = []
-    for i in f:
-        print(i)
-        a, b, c = i.split()
-        x.append(float(a))
-        y.append(float(b))
-        weights.append(float(c))
+with open("data.txt", "r") as file:
+    for line in file.readlines():
+        line = line.split()
+        if len(line) != 3:
+            continue
+        else:
+            x.append(float(line[0]))
+            y.append(float(line[1]))
+            p.append(float(line[2]))
 
-    return x, y, weights, order
+if len(x) == 0:
+    print("Table is NULL!Error\n")
+    exit()
+else:
+    print("Table with len ", len(x), ":")
+    print('x          |       y    |        weights')
+    print('-'*40)
+    for i in range(len(x)):
+        print('{:>10.5f} | {:>10.5f} | {:>10.5f}'.format(x[i], y[i], p[i]))
+    print()
+    
+n = int(input('input polinomial order: '))
+
+coefs = solve()
 
 
-def main():
-    x, y, weights, order = inputData();
-    print(x, y, weights, order);
+x_show = [min(x) - (max(x) - min(x)) / 10 + i * 1.2 * (max(x) - min(x)) / 1000 for i in range(1000)]
+plt.plot(x, y, 'ko')
+plt.plot(x_show, [approximate(i, coefs) for i in x_show], '-r')
 
-
-if __name__ == "__main__":
-    main()
+plt.show()
--- lab_04/randGen.py
+++ lab_04/randGen.py
@@ -1,7 +1,16 @@
 import random as r
 
+def f(x):
+    return x**3+x+12
 
-f = open("data.txt", "w")
+def write_file():
+    f_add = open("data.txt", "w")
+    for i in range(20):
+        x = -5 + i * 0.5
+        # +-7%
+        f_add.write("{:0.4f} {:0.4f} {:0.4f}\n".format(x, f(x) * (1 + (r.random() - 5.7) *
+0.1), 1))
+    return
 
-for i in range(30):
-    f.write(str(r.uniform(10, 90)) + " " + str(r.uniform(10,90)) + " " + str(r.uniform(0, 1)) + "\n")
+if __name__ == "__main__":
+    write_file()
